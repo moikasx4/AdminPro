cdc709d484401667c31aa36673616a973f5de3ee
moises <mestrada@asgbpm.com>
2020-09-17 14:22:08 -0500

    Fin seccion 4 - Optimizacion de Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule} from '@angular/router'
+import { SharedModule } from '../shared/shared.module';
+
+
+//Component
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
